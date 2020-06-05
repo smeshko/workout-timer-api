d3ca393790b3d7ce99a5398f8435c03920b92836
Ivaylo Tsonev <itsonev@ebay.com>
2020-06-05 12:03:56 +0200

    Update Dtos

--- Sources/WorkoutTimerAPI/Models/WorkoutListDto.swift
+++ Sources/WorkoutTimerAPI/Models/WorkoutListDto.swift
@@ -6,15 +6,13 @@ public struct WorkoutListDto: Codable {
     public var name: String
     public var imageKey: String
     public var thumbnailKey: String
-    public var totalDuration: Int
-    public var exerciseCount: Int
-    
-    public init(id: String, name: String, imageKey: String, thumbnailKey: String, totalDuration: Int, exerciseCount: Int) {
+    public var exerciseSets: [ExerciseSetListDto]
+
+    public init(id: String, name: String, imageKey: String, thumbnailKey: String, exerciseSets: [ExerciseSetListDto]) {
         self.id = id
         self.name = name
         self.imageKey = imageKey
         self.thumbnailKey = thumbnailKey
-        self.totalDuration = totalDuration
-        self.exerciseCount = exerciseCount
+        self.exerciseSets = exerciseSets
     }
 }
