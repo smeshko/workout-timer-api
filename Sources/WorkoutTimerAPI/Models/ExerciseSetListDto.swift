c9cfa1267ecce9ab32994ed6449bb0e563fb7b47
Ivaylo Tsonev <itsonev@ebay.com>
2020-07-20 22:29:25 +0300

    Add type to exercise set

--- Sources/WorkoutTimerAPI/Models/ExerciseSetListDto.swift
+++ Sources/WorkoutTimerAPI/Models/ExerciseSetListDto.swift
@@ -5,10 +5,12 @@ public struct ExerciseSetListDto: Codable {
     public var id: String
     public var duration: Double
     public var exercise: ExerciseGetDto?
+    public var type: String
     
-    public init(id: String, duration: Double, exercise: ExerciseGetDto? = nil) {
+    public init(id: String, duration: Double, exercise: ExerciseGetDto? = nil, type: String) {
         self.id = id
         self.duration = duration
         self.exercise = exercise
+        self.type = type
     }
 }
